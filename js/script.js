8e3b5951640d9581a81d94828a72c5f366edf445
Scott Chou <iscott.chou@gmail.com>
2016-10-19 18:27:32 -0500

    Edit JS file

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
-  // $('#about').on('click',function(){
-  //   var hajin = $('#aboutSection').offsetTop;
-  //   $(document).scrollTop(hajin);
-  // })
 
-  $(window).scroll(function(event){
-
-    var spacing = $('#skillsSection').offset().top - $('.navbar').outerHeight();
-    var currentSpacing = $(document).scrollTop();
-    var diff = currentSpacing/spacing > 1 ? 1:currentSpacing/spacing;
-    var diff2 = 1 - diff;
-    var textColor = Math.floor(255-(255*diff));
-
-    $('.navbar').css("background-color","rgba(255,255,255,"+diff+")");
-    $('.navbar').css("color","rgb("+textColor+","+textColor+","+textColor+")");
-
-
-  });
-
-  $('#skills').on('click',function(){
-    var spacing = $('#skillsSection').offset().top - $('.navbar').outerHeight();
-    $('html,body').animate({scrollTop:spacing});
-  });
-
+  $('#about').on('click',function(){
+    $("#aboutSection").show();
+    $("#projectSection").hide();
+    $("#blogSection").hide();
+    $('#contactSection').hide();
+  })
+  $('#projects').on('click',function(){
+    $("#aboutSection").hide();
+    $("#projectSection").show();
+    $("#blogSection").hide();
+    $('#contactSection').hide();
+  })
+  $('#blog').on('click',function(){
+    $("#aboutSection").hide();
+    $("#projectSection").hide();
+    $("#blogSection").show();
+    $('#contactSection').hide();
+  })
+  $('#contact').on('click',function(){
+    $("#aboutSection").hide();
+    $("#projectSection").hide();
+    $("#blogSection").hide();
+    $('#contactSection').show();
+  })
 });
